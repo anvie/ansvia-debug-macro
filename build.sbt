0d3baff3366f9dfa808da5056d316ea5ef6850c0
robin <robin@ansvia.com>
2015-11-24 13:23:42 +0700

    first init

---
+++ build.sbt
@@ -0,0 +1,65 @@
+//enablePlugins(ScalaJSPlugin)
+
+import sbt._
+
+import Keys._
+
+//import org.scalajs.sbtplugin.ScalaJSPlugin.autoImport._
+
+organization := "com.ansvia.macros"
+
+name := "ansvia-debug"
+
+version := "0.0.5-SNAPSHOT"
+
+description := ""
+
+scalaVersion := "2.11.6"
+
+//crossScalaVersions := Seq("2.10.4")
+
+resolvers ++= Seq(
+	"Sonatype Releases" at "https://oss.sonatype.org/content/groups/scala-tools",
+	"typesafe repo"   at "http://repo.typesafe.com/typesafe/releases",
+	"Ansvia release repo" at "http://scala.repo.ansvia.com/releases",
+	"Ansvia snapshot repo" at "http://scala.repo.ansvia.com/nexus/content/repositories/snapshots"
+)
+
+//libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-compiler" % _)
+
+libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-reflect" % _)
+
+// libraryDependencies ++= Seq(
+//     "org.specs2" %% "specs2" % "1.14",
+//     "ch.qos.logback" % "logback-classic" % "1.0.13"
+// )
+
+//enable this if eclipse plugin activated
+//EclipseKeys.withSource := true
+
+
+publishTo <<= version { (v:String) =>
+    val ansviaRepo = "http://scala.repo.ansvia.com/nexus"
+    if(v.trim.endsWith("SNAPSHOT"))
+        Some("snapshots" at ansviaRepo + "/content/repositories/snapshots")
+    else
+        Some("releases" at ansviaRepo + "/content/repositories/releases")
+}
+
+credentials += Credentials(Path.userHome / ".ivy2" / ".credentials")
+
+publishArtifact in Test := false
+
+pomIncludeRepository := { _ => false }
+
+crossPaths := true
+
+pomExtra := (
+  <url>http://ansvia.com</url>
+  <developers>
+    <developer>
+      <id>anvie</id>
+      <name>Robin Sy</name>
+      <url>http://www.mindtalk.com/u/robin</url>
+    </developer>
+  </developers>)
---
+++ src/main/scala/com/ansvia/macros/DebugMacros.scala
@@ -0,0 +1,72 @@
+package com.ansvia.macros
+
+
+import language.experimental.macros
+import scala.reflect.macros.Context
+
+trait DebugMacros {
+    def info(params: Any*):Unit = macro DebugMacros.info_impl
+    def debug(params: Any*):Unit = macro DebugMacros.debug_impl
+    def warn(params: Any*):Unit = macro DebugMacros.warn_impl
+    def error(params: Any*):Unit = macro DebugMacros.error_impl
+}
+
+object DebugMacros extends DebugMacros {
+
+    private val isDisabled = {
+        System.getenv("ANSVIA_DEBUG") match {
+            case null => false
+            case x if x.trim().toLowerCase == "false" =>
+                true
+            case _ => false
+        }
+    }
+
+    def info_impl(c:Context)(params:c.Expr[Any]*):c.Expr[Unit] = _print_impl("[INFO]: ")(c)(params: _*)
+    def debug_impl(c:Context)(params:c.Expr[Any]*):c.Expr[Unit] = _print_impl("[DEBUG]: ")(c)(params: _*)
+    def warn_impl(c:Context)(params:c.Expr[Any]*):c.Expr[Unit] = _print_impl("[WARN]: ")(c)(params: _*)
+    def error_impl(c:Context)(params:c.Expr[Any]*):c.Expr[Unit] = _print_impl("[ERROR]: ")(c)(params: _*)
+
+    def _print_impl(prefix:String)(c:Context)(params:c.Expr[Any]*):c.Expr[Unit] = {
+        import c.universe._
+
+        val prefixExpr = c.Expr[String](Literal(Constant(prefix)))
+
+        if (!isDisabled || prefix == "[ERROR]: "){
+
+            val trees = params.map { param =>
+                param.tree match {
+                    case Literal(Constant(_const)) => {
+                        val reified = reify {
+                            print(prefixExpr.splice + param.splice)
+                        }
+                        reified.tree
+                    }
+                    case _ => {
+                        val paramRep = show(param.tree)
+                        val paramRepTree = Literal(Constant(paramRep))
+                        val paramRepExpr = c.Expr[String](paramRepTree)
+                        val reified = reify {
+                            print(prefixExpr.splice + paramRepExpr.splice + " = " + param.splice)
+                        }
+                        reified.tree
+                    }
+                }
+            }
+
+            val seps = (1 to trees.size-1).map(_ => reify {
+                print(", ")
+            }.tree) :+ reify {
+                println()
+            }.tree
+
+            val treeSeps = trees.zip(seps).flatMap(p => List(p._1, p._2))
+
+            c.Expr[Unit](Block(treeSeps.toList, Literal(Constant(()))))
+
+        }else{
+            reify { () }
+        }
+    }
+}
+
